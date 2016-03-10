2a6590138fdd5e8efd22a0046a5a02e55307dc4c
giftsoccer14 <giftsoccer14@gmail.com>
2016-03-10 11:05:02 -0500

    First stable release

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 var main = function() {
  
+  var dataArr=[];
+  
   for (i = 1; i < 9; i+= 1) {
     if (localStorage.getItem($('#' + i).prev().text()) === null) {
      $('#' + i).text(0);
      localStorage.setItem($('#' + i).prev().text(), 0);
+     dataArr.push([$('#' + i).prev().text(), 0]);
   } else {
     $('#' + i).text(localStorage.getItem($('#' + i).prev().text()));
+    dataArr.push([$('#' + i).prev().text(), parseInt($('#' + i).text())]);
   }
   }
+  
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Restaurant');
+        data.addColumn('number', 'Times');
+        
+        data.addRows(dataArr);
+
+        var options = {
+          title: 'Restaurant Breakdown',
+          is3D: true,
+          chartArea: {left:30,top:70,width:'100%',height:'100%'},
+          width:550,
+          /*titleTextStyle: {color: 'black', fontName: Georgia, fontSize: 10},*/
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
   
   $('#8').text(localStorage.getItem($('#8').prev().text()));
   $('#date').text(localStorage.getItem('date'));
